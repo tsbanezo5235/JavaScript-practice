9667630f74d8b0233238d4f314429c2541c8dc69
Yu Jia Liao <kevinc0227@gmail.com>
2021-01-31 20:48:07 +0800

    complete HW2

---
+++ HW2.js
@@ -0,0 +1,13 @@
+"use strict";
+let admin,
+    name;
+    name = "John";
+admin = name;
+alert(admin);
+
+// Create a variable with the name of our planet. 
+// How would you name such a variable?
+let planetName = "Earth";
+// Create a variable to store the name of a current visitor to a website.
+// How would you name that variable?
+let currentUserName = "XXX";

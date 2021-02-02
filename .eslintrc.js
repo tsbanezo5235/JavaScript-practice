516fca9cfdbd5881afb33d4a19ea0050a37d293f
Yu Jia Liao <kevinc0227@gmail.com>
2021-02-02 21:39:34 +0800

    try a new way to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
+        "plugin:react/recommended",
+        "airbnb"
     ],
     "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
         "ecmaVersion": 12,
         "sourceType": "module"
     },
     "plugins": [
-        "vue"
+        "react"
     ],
     "rules": {
     }
